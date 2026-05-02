e10b488db3d97e82e37e160d75a0f1ab3a89870a
YagneshGuduru <yagnesh2403@gmail.com>
2026-05-02 10:40:50 +0200

    Load an audio file.

---
+++ test_audio.py
@@ -0,0 +1,16 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+import soundfile as sf
+
+audio_file_path = "data/speech_commands_v0.02/happy/0a2b400e_nohash_0.wav"
+
+# Load audio file.
+audio_data, sample_rate = librosa.load(audio_file_path, sr = None)
+# audio_data, sample_rate = sf.read(audio_file_path)
+
+print(f"Sampe rate: {sample_rate} Hz")
+print(f"Number of samples: {len(audio_data)}")
+print(f"Duration: {len(audio_data) / sample_rate: .2f} seconds")
+
